4f13edacb5e3c9034b654b31271ba4760a0a4b5d
vovaanik <69124723+vovaanik@users.noreply.github.com>
2020-09-03 17:29:51 -0500

    Add files via upload
    DZ 2-1

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int Savings;
+        Savings = 2_000_000_000;
+        System.out.println(Savings);
+        int Transfer;
+        Transfer = 500_000_000;
+        System.out.println(Transfer);
+        int balance = Savings + Transfer;
+        System.out.println(balance);
+    }
+}
+
